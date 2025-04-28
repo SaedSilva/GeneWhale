43289b52a875f10a8a2f2b615d24624cc8089ad0
Saed Sousa <saed.sousa@hotmail.com>
2025-04-27 22:24:33 -0300

    Refactor build configuration to use version catalog for dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -14,26 +14,22 @@ kotlin {
         val desktopMain by getting
         
         commonMain.dependencies {
-            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+            implementation(libs.kotlinx.datetime)
 
             implementation(libs.kotlin.test)
 
-            val fileKitVersion = "0.10.0-beta02"
-            implementation("io.github.vinceglb:filekit-core:$fileKitVersion")
-            implementation("io.github.vinceglb:filekit-dialogs:$fileKitVersion")
-            implementation("io.github.vinceglb:filekit-dialogs-compose:$fileKitVersion")
-            implementation("io.github.vinceglb:filekit-coil:$fileKitVersion")
+            implementation(libs.filekit.core)
+            implementation(libs.filekit.dialogs)
+            implementation(libs.filekit.dialogs.compose)
+            implementation(libs.filekit.coil)
 
-//            implementation("io.ktor:ktor-client-core:3.1.2")
-//            implementation("io.ktor:ktor-client-cio:3.1.2")
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.koin.compose.viewmodel.navigation)
 
-            val koinVersion = "4.0.3"
-            implementation("io.insert-koin:koin-compose:$koinVersion")
-            implementation("io.insert-koin:koin-compose-viewmodel:$koinVersion")
-            implementation("io.insert-koin:koin-compose-viewmodel-navigation:$koinVersion")
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.navigation.compose)
 
-            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
-            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.8.0-alpha12")
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
